4c56a431d1ad36fcaa780eeaf92ddd974a1e32d6
MadaAlkheraiji <114874260+MadaAlkheraiji@users.noreply.github.com>
2022-10-02 20:28:32 +0300

    Add files via upload

---
+++ Kotlin.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+fun main() {
+
+
+    var name:String="abduallah"
+    var a :Float =20.6f
+    var b:Double=8.947362
+    var c :Int =25
+    var d: Int =20200000
+    var e:Double=32345947.0
+    var f =true
+    var g :Char= 'a'
+    var r=9
+    var t=8
+    println(" ${c++}  Hello, world!!! ${c++}")
+    println((r*t))
+}
