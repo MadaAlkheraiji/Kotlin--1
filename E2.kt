0bae09173b40eccaab86f6d537a0c49cdc35cc77
MadaAlkheraiji <114874260+MadaAlkheraiji@users.noreply.github.com>
2022-10-04 17:02:36 +0300

    Add files via upload

---
+++ E2.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+fun main() {
+
+
+    var flag:Boolean=(false&&false)
+    var a:Double=10.0%3.3
+    var flag2:Boolean=(5>=5)
+    var flag3:Boolean=(false||true)
+    var b:Int=5-3*2
+    var flag4:Boolean=(3!=2)
+    var c:Int=(6+6)/3
+
+    println("$flag \n $a \n $flag2 \n $flag3 \n $b \n $flag4 \n $c ")
+
+}
